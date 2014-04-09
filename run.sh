94117f0cd3d3c489e10fc76ab2fd34a831be11cb
Morgante Pell <morgante.pell@morgante.net>
2014-04-09 13:36:29 +0400

    try to keep container running

--- run.sh
+++ run.sh
@@ -3,6 +3,6 @@
 ssh -oStrictHostKeyChecking=no -f -N -D 0.0.0.0:1080 localhost
 /spiped/spiped/spiped -d -s '[0.0.0.0]:8089' -t '[127.0.0.1]:1080' -k  /spiped/spiped.key
 
-touch /var/log
+touch /var/log/spiped
 
-tail -f /var/log
+tail -f /var/log/spiped
